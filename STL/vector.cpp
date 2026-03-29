6a6db0218a51128256a7cdd3d9f108a9c8954e2f
piyush -gitproeditor <piyushsaini3909@gmail.com>
2026-03-29 15:29:14 +0530

    this is the vector with an iterator inside it

---
+++ STL/vector.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    vector<int>:: iterator it= v.begin();
+    it++;
+    cout<<*(it)<<" ";
+    
+
+
+    return 0;
+}
