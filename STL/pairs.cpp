b00d52aec64b0a7cffda092a7b2aa1b8f1939f5f
piyush -gitproeditor <piyushsaini3909@gmail.com>
2026-03-29 14:33:45 +0530

    this is the pair function of stl

---
+++ STL/pairs.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    pair<int,int> p;
+    p.first =10;
+    p.second = 20;
+    cout<< "the first number is "<< p.first << " and the second number is "<< p.second ;
+
+    return 0;
+
+
+}
