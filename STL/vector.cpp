5dc85d50b15488a7f673565498f805bc30fe478d
piyush -gitproeditor <piyushsaini3909@gmail.com>
2026-04-01 18:41:33 +0530

    this is the vector function in STL and an iterator for accesing all its elements

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -2,16 +2,9 @@
 using namespace std;
 int main(){
     vector<int> v={10,20,30};
+    for (vector<int>::iterator it = v.begin();it!=v.end(); it++){
+        cout<<*(it)<<" ";
+    }
     
-    vector<int>:: iterator it= v.begin();
-    
-    cout<<*(it)<<" ";
-    it++;
-     cout<<*(it)<<" ";
-    it++;
-      cout<<*(it)<<" ";
-    
-
-
     return 0;
 }
