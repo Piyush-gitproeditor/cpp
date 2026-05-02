d74b97576381358ebd348e8334ef3576aa1bb4d0
piyush -gitproeditor <piyushsaini3909@gmail.com>
2026-05-02 15:32:29 +0530

    Update hashing files

--- data structures and algo/hashing.cpp
+++ data structures and algo/hashing.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main(){
     string s;
     cin>>s;
-    int hash[26]={0};
+    int hash[256]={0};
     for(int i=0;i<s.size();i++){
-        hash[s[i]-'a'] ++;
+        hash[s[i]] ++;
     }
     int q;
     cin>>q;
@@ -13,8 +13,8 @@ int main(){
         char c;
         cin>>c;
 
-        cout<<hash[c-'a']<<endl;
+        cout<<hash[c]<<endl;
     }
 
     return 0;
-}
+}   
