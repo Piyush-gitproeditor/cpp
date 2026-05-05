bc9899b171dc41d2a5bb3b0b04ceb1f124f5f1a9
piyush -gitproeditor <piyushsaini3909@gmail.com>
2026-05-05 14:36:57 +0530

    this is the hashing of the array

---
+++ data structures and algo/hashing_mapping.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    map<int ,int> mpp;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        mpp[arr[i]]++;
+    }
+}
