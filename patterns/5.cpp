9706bcfd79d5e6551997cfd5a3bccd2c13739862
piyush -gitproeditor <piyushsaini3909@gmail.com>
2026-05-07 19:25:32 +0530

    this is a patterns

---
+++ patterns/5.cpp
@@ -0,0 +1,35 @@
+/*
+            *
+           ***
+          *****
+         *******
+        *********   
+
+*/
+
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter n: ";
+    cin>>n;
+    for (int i=0;i<5;i++){
+        for (int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for (int j=0;j<2*i+1;j++){
+            cout<<"*";
+               
+            
+            }
+             for (int j=0;j<n-i-1;j++){
+                cout<<"  ";
+                }
+                cout<<endl;
+    }
+    
+    
+    return 0;
+}
