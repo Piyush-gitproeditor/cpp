13d842edd30fee34a7d482759e3e424f587d8746
piyush -gitproeditor <piyushsaini3909@gmail.com>
2026-05-07 18:09:01 +0530

    patterns

--- patterns/3.cpp
+++ patterns/3.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     for (int i=0;i<5;i++){
         for (int j=0;j<=5;j++){
-            cout<<" * "<<j;
+            cout<<;
         }
         cout<<endl;
     }
